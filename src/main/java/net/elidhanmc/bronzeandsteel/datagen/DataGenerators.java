a0cc7e70a130f9c7bb1fc77c9319917739e90f87
elidhan <84767537+elidhan@users.noreply.github.com>
2026-08-29 22:38:10 +0800

    Update DataGenerators.java

--- src/main/java/net/elidhanmc/bronzeandsteel/datagen/DataGenerators.java
+++ src/main/java/net/elidhanmc/bronzeandsteel/datagen/DataGenerators.java
@@ -1,28 +1,3 @@
-/*
- * Portions of this file are based on work from [Project Name],
- * Copyright (c) 2024 Kaupenjoe.
- * Licensed under the MIT License.
- *
- * The MIT License:
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package net.elidhanmc.bronzeandsteel.datagen;
 
 import net.elidhanmc.bronzeandsteel.BronzeAndSteel;
@@ -37,6 +12,11 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
 
+/*
+ * Based on work from Kaupenjoe's NeoForge 1.21.X tutorial series,
+ * Copyright (c) 2024 Kaupenjoe.
+ * Licensed under the MIT License.
+ */
 
 @EventBusSubscriber(modid = BronzeAndSteel.MODID)
 public class DataGenerators {
